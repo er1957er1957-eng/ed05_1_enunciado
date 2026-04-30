12c55efcc80d239f5edebc7909ffd52ae28f0959
Rubén <er1957er1957@gmail.com>
2026-04-30 19:34:45 +0200

    añadidos test

--- src/test/java/org/ed05_1/GestorJuegosTest.java
+++ src/test/java/org/ed05_1/GestorJuegosTest.java
@@ -1,4 +1,63 @@
 package org.ed05_1;
 
-public class GestorJuegosTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GestionJuegosTest {
+
+    private GestorJuegos gestor;
+
+    @BeforeEach
+    void setUp() {
+        gestor = new GestorJuegos();
+
+        gestor.registrarLoteJuegos(
+                new String[]{"ABC123"},
+                new int[]{10}
+        );
+    }
+
+    @Test
+    void ventaCorrecta() {
+        int resultado = gestor.venderJuego("ABC123", 2);
+        assertEquals(2, resultado);
+    }
+
+    @Test
+    void cantidadCero() {
+        int resultado = gestor.venderJuego("ABC123", 0);
+        assertEquals(0, resultado);
+    }
+
+    @Test
+    void cantidadNegativa() {
+        int resultado = gestor.venderJuego("ABC123", -1);
+        assertEquals(0, resultado);
+    }
+
+    @Test
+    void stockInsuficiente() {
+        int resultado = gestor.venderJuego("ABC123", 11);
+        assertEquals(-2, resultado);
+    }
+
+    @Test
+    void juegoNoExiste() {
+        int resultado = gestor.venderJuego("XYZ999", 2);
+        assertEquals(-1, resultado);
+    }
+
+    @Test
+    void codigoInvalidoLongitud() {
+        int resultado = gestor.venderJuego("ABC12", 2);
+        assertEquals(0, resultado);
+    }
+
+    @Test
+    void codigoInvalidoMinusculas() {
+        int resultado = gestor.venderJuego("abc123", 2);
+        assertEquals(0, resultado);
+    }
 }
